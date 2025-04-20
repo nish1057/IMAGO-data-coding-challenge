5a53df89f894f52158b697af39b6b5a1e1cf7457
Nishtha Narayan <51740842+nish1057@users.noreply.github.com>
2025-04-20 23:44:44 +0200

    Add files via upload

---
+++ dbt/models/staging/stg_positions.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='view') }}
+
+SELECT
+    id,
+    ReId,
+    KdNr,
+    Nettobetrag,
+    Bildnummer,
+    CAST(VerDatum AS DATE) AS VerDatum,
+    CASE
+        WHEN Bildnummer = 100000000 THEN TRUE
+        ELSE FALSE
+    END AS is_placeholder_media
+FROM {{ source('raw', 'Abrechnung_Positionen') }}
